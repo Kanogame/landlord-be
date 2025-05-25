197e25427bbff16f8598715658f7102f85b2f837
kanogame <wasdqert1222@gmail.com>
2025-05-25 16:04:56 +0700

    works

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -1,39 +1,45 @@
 using landlord_be.Models;
 
-namespace landlord_be.Data {
-    public class DbInitializer {
+namespace landlord_be.Data
+{
+    public class DbInitializer
+    {
         private readonly ILogger _logger;
 
-        public DbInitializer(ILogger logger) {
+        public DbInitializer(ILogger logger)
+        {
             _logger = logger;
         }
 
-        public void Initialize(ApplicationDbContext context) {
+        public void Initialize(ApplicationDbContext context)
+        {
             context.Database.EnsureCreated();
 
-            if (context.Users.Any()) {
+            if (context.Users.Any())
+            {
                 return;
             }
 
+            DateTime time = DateTime.Now.ToUniversalTime();
             var users = new List<User>
-        {
-            new User { Name = "Иван Иванов", PasswordHash = "hash1", Token = "token1", RegisterDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new User { Name = "Петр Петров", PasswordHash = "hash2", Token = "token2", RegisterDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new User { Name = "Сергей Сергеев", PasswordHash = "hash3", Token = "token3", RegisterDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new User { Name = "Алексей Алексеев", PasswordHash = "hash4", Token = "token4", RegisterDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new User { Name = "Дмитрий Дмитриев", PasswordHash = "hash5", Token = "token5", RegisterDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new User { Name = "Анна Антонова", PasswordHash = "hash6", Token = "token6", RegisterDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new User { Name = "Мария Маркова", PasswordHash = "hash7", Token = "token7", RegisterDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new User { Name = "Елена Еленина", PasswordHash = "hash8", Token = "token8", RegisterDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new User { Name = "Олег Олегов", PasswordHash = "hash9", Token = "token9", RegisterDate = DateTime.Now, UpdateDate = DateTime.Now },
-            new User { Name = "Татьяна Татьянова", PasswordHash = "hash10", Token = "token10", RegisterDate = DateTime.Now, UpdateDate = DateTime.Now }
-        };
+            {
+                new User { Name = "Иван Иванов", PasswordHash = "hash1", Token = "token1", RegisterDate = time, UpdateDate = time },
+            new User { Name = "Петр Петров", PasswordHash = "hash2", Token = "token2", RegisterDate = time, UpdateDate = time },
+            new User { Name = "Сергей Сергеев", PasswordHash = "hash3", Token = "token3", RegisterDate = time, UpdateDate = time },
+            new User { Name = "Алексей Алексеев", PasswordHash = "hash4", Token = "token4", RegisterDate = time, UpdateDate = time },
+            new User { Name = "Дмитрий Дмитриев", PasswordHash = "hash5", Token = "token5", RegisterDate = time, UpdateDate = time },
+            new User { Name = "Анна Антонова", PasswordHash = "hash6", Token = "token6", RegisterDate = time, UpdateDate = time },
+            new User { Name = "Мария Маркова", PasswordHash = "hash7", Token = "token7", RegisterDate = time, UpdateDate = time },
+            new User { Name = "Елена Еленина", PasswordHash = "hash8", Token = "token8", RegisterDate = time, UpdateDate = time },
+            new User { Name = "Олег Олегов", PasswordHash = "hash9", Token = "token9", RegisterDate = time, UpdateDate = time },
+            new User { Name = "Татьяна Татьянова", PasswordHash = "hash10", Token = "token10", RegisterDate = time, UpdateDate = time },
+            };
 
-        users.ForEach(u => context.Users.Add(u));
-        context.SaveChanges();
+            users.ForEach(u => context.Users.Add(u));
+            context.SaveChanges();
 
-        // Create properties for each user
-        var properties = new List<Property>
+            // Create properties for each user
+            var properties = new List<Property>
         {
             new Property { OwnerId = users[0].Id, Name = "Квартира в центре", Desc = "Уютная квартира с видом на парк", Address = "Центральная улица, 1", Area = 50, ImageLinkId = 1 },
             new Property { OwnerId = users[0].Id, Name = "Дом с садом", Desc = "Просторный дом с большим садом", Address = "Садовая улица, 10", Area = 150, ImageLinkId = 2 },
@@ -50,10 +56,10 @@ namespace landlord_be.Data {
             new Property { OwnerId = users[9].Id, Name = "Дача с участком", Desc = "Дача с большим участком земли", Address = "Участковая улица, 14", Area = 110, ImageLinkId = 13 }
         };
 
-        properties.ForEach(p => context.Properties.Add(p));
-        context.SaveChanges();
+            properties.ForEach(p => context.Properties.Add(p));
+            context.SaveChanges();
 
-        _logger.LogInformation("Database was populated with fake data");
+            _logger.LogInformation("Database was populated with fake data");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,46 +1,46 @@
-//var builder = WebApplication.CreateBuilder(args);
-//
-//// Add services to the container.
-//
-//builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-//
-//var app = builder.Build();
-//
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-//
-//app.UseHttpsRedirection();
-//
-//app.UseAuthorization();
-//
-//app.MapControllers();
-//
-//app.Run();
-//
+using landlord_be.Data;
+using Microsoft.EntityFrameworkCore;
 
-using landlord_be;
-using Microsoft.AspNetCore;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace Todo2Api
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+    option.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection"))
+);
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
 {
-    public class Program
+    var services = scope.ServiceProvider;
+    try
     {
-         public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://localhost:5001")
-                .Build();
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        var dbInit = new DbInitializer(app.Logger);
+        dbInit.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
     }
 }
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
